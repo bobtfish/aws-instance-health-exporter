b15b8b363547b38af035de1b4d14065c25540250
Tomas Doran <tdoran@yelp.com>
2020-05-25 08:07:19 -0700

    Does stuff

--- aws_instance_health_exporter.go
+++ aws_instance_health_exporter.go
@@ -14,8 +14,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
-	"github.com/aws/aws-sdk-go/service/health"
-	"github.com/aws/aws-sdk-go/service/health/healthiface"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -24,19 +22,6 @@ import (
 )
 
 const (
-	// APIRegion is the region where the AWS health api lives. Currently
-	// this is only `us-east-1`, see: http://docs.aws.amazon.com/health/latest/ug/getting-started-api.html
-	APIRegion = "us-east-1"
-
-	// LabelCategory defines the event type category of the event, e.g. issue, accountNotification, scheduledChange
-	LabelCategory = "category"
-	// LabelRegion defines the region of the event, e.g. us-east-1
-	LabelRegion = "region"
-	// LabelService defines the service of the event, e.g. EC2, RDS
-	LabelService = "service"
-	// LabelStatusCode defines the status of the event, e.g. open, upcoming, closed
-	LabelStatusCode = "status_code"
-	// Namespace is the metrics prefix
 	Namespace = "aws_instance_health"
 )
 
@@ -46,63 +31,58 @@ var (
 	// Version represents the Build SHA-1 of the binary
 	Version = "N/A"
 
-	// labels are the static labels that come with every metric
-	labels = []string{LabelCategory, LabelRegion, LabelService, LabelStatusCode}
-
-	// events is the number of aws health events reported
-	eventOpts = prometheus.GaugeOpts{
-		Name:      "events",
-		Namespace: Namespace,
-		Help:      "Gauge for aws health events",
-	}
+	instanceEvents *prometheus.Desc
+	statusOk       *prometheus.Desc
 )
 
 type exporter struct {
-	api    healthiface.HealthAPI
 	client ec2iface.EC2API
-	filter *health.EventFilter
 }
 
 func (e *exporter) Describe(ch chan<- *prometheus.Desc) {
-	ch <- prometheus.NewDesc(
-		prometheus.BuildFQName(eventOpts.Namespace, eventOpts.Subsystem, eventOpts.Name),
-		eventOpts.Help,
-		labels,
-		nil,
-	)
+	ch <- instanceEvents
+	ch <- statusOk
 }
 
 func (e *exporter) Collect(ch chan<- prometheus.Metric) {
-	gv := prometheus.NewGaugeVec(eventOpts, labels)
-	e.scrapeHealth(gv)
-	gv.Collect(ch)
-}
-
-func (e *exporter) scrapeHealth(gv *prometheus.GaugeVec) {
-	var events []*health.Event
-
-	err := e.api.DescribeEventsPages(&health.DescribeEventsInput{
-		Filter: e.filter,
-	}, func(out *health.DescribeEventsOutput, lastPage bool) bool {
-		events = append(events, out.Events...)
-		return true
-	})
-
-	if err != nil {
-		log.Println(err)
-		return
+	allIs := make([]*ec2.InstanceStatus, 0)
+	more := true
+	var nextToken *string
+	for more == true {
+		is, err := e.client.DescribeInstanceStatus(&ec2.DescribeInstanceStatusInput{
+			IncludeAllInstances: aws.Bool(true),
+			MaxResults:          aws.Int64(1000),
+			NextToken:           nextToken,
+		})
+		if err != nil {
+			panic(err)
+		}
+		for _, s := range is.InstanceStatuses {
+			fmt.Println("instance %s instance state %s system status %s", *s.InstanceId, *s.InstanceStatus.Status, *s.SystemStatus.Status)
+			allIs = append(allIs, s)
+		}
+		if is.NextToken == nil {
+			more = false
+		} else {
+			nextToken = is.NextToken
+		}
 	}
-
-	for _, e := range events {
-		gv.WithLabelValues(
-			aws.StringValue(e.EventTypeCategory),
-			aws.StringValue(e.Region),
-			aws.StringValue(e.Service),
-			aws.StringValue(e.StatusCode)).Inc()
-	}
+	ch <- prometheus.MustNewConstMetric(instanceEvents, prometheus.GaugeValue, float64(1), "i-1234")
 }
 
 func init() {
+	instanceEvents = prometheus.NewDesc(
+		prometheus.BuildFQName(Namespace, "health", "events"),
+		"events for instances",
+		[]string{"instance_id"},
+		nil,
+	)
+	statusOk = prometheus.NewDesc(
+		prometheus.BuildFQName(Namespace, "health", "status_ok"),
+		"Health status for instance",
+		[]string{"health_type", "instance_id"},
+		nil,
+	)
 	prometheus.MustRegister(version.NewCollector("aws_instance_health_exporter"))
 }
 
@@ -110,9 +90,7 @@ func main() {
 	var (
 		showVersion = kingpin.Flag("version", "Print version information").Bool()
 		listenAddr  = kingpin.Flag("web.listen-address", "The address to listen on for HTTP requests.").Default(":9165").String()
-		categories  = kingpin.Flag("aws.category", "A list of event type category codes (issue, scheduledChange, or accountNotification) that are used to filter events.").Strings()
-		regions     = kingpin.Flag("aws.region", "A list of AWS regions that are used to filter events").Strings()
-		services    = kingpin.Flag("aws.service", "A list of AWS services that are used to filter events").Strings()
+		region      = kingpin.Flag("aws.region", "The AWS region").Default("us-east-1").String()
 	)
 
 	registerSignals()
@@ -130,23 +108,12 @@ func main() {
 
 	log.Printf("Starting `aws-instance-health-exporter`: Build Time: '%s' Build SHA-1: '%s'\n", BuildTime, Version)
 
-	sess, err := session.NewSession(&aws.Config{Region: aws.String(APIRegion)})
+	sess, err := session.NewSession(&aws.Config{Region: region})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	filter := &health.EventFilter{}
-	if len(*categories) > 0 {
-		filter.EventTypeCategories = aws.StringSlice(*categories)
-	}
-	if len(*regions) > 0 {
-		filter.Regions = aws.StringSlice(*regions)
-	}
-	if len(*services) > 0 {
-		filter.Services = aws.StringSlice(*services)
-	}
-
-	exporter := &exporter{api: health.New(sess), client: ec2.New(sess), filter: filter}
+	exporter := &exporter{client: ec2.New(sess)}
 	prometheus.MustRegister(exporter)
 
 	mux := http.NewServeMux()
