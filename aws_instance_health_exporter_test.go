b89f5726dd2318c72e91a9d8f8f5eeffacbd49f3
Tomas Doran <tdoran@yelp.com>
2020-05-25 08:44:31 -0700

    Strip old stuff from test

--- aws_instance_health_exporter_test.go
+++ aws_instance_health_exporter_test.go
@@ -3,86 +3,66 @@ package main
 import (
 	"testing"
 
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/ec2/ec2iface"
-	"github.com/aws/aws-sdk-go/service/health"
-	"github.com/aws/aws-sdk-go/service/health/healthiface"
-	"github.com/prometheus/client_golang/prometheus"
-	dto "github.com/prometheus/client_model/go"
 )
-
-type mockHealthAPI struct {
-	healthiface.HealthAPI
-	events []*health.Event
-}
-
-func (api *mockHealthAPI) DescribeEventsPages(in *health.DescribeEventsInput, fn func(*health.DescribeEventsOutput, bool) bool) error {
-	output := health.DescribeEventsOutput{Events: api.events}
-	fn(&output, false)
-	return nil
-}
 
 type mockEC2Client struct {
 	ec2iface.EC2API
+	statuses []*ec2.InstanceStatus
 }
 
 func (client *mockEC2Client) DescribeInstanceStatus(in *ec2.DescribeInstanceStatusInput) (*ec2.DescribeInstanceStatusOutput, error) {
-	statuses := make([]*ec2.InstanceStatus, 0)
 	output := ec2.DescribeInstanceStatusOutput{
-		InstanceStatuses: statuses,
+		InstanceStatuses: client.statuses,
 		NextToken:        nil,
 	}
 	return &output, nil
 }
 
 func TestScrape(t *testing.T) {
-	var events = []*health.Event{
-		&health.Event{
-			EventTypeCategory: aws.String("issue"),
-			Region:            aws.String("eu-west-1"),
-			Service:           aws.String("EC2"),
-			StatusCode:        aws.String("open"),
-		},
-		&health.Event{
-			EventTypeCategory: aws.String("issue"),
-			Region:            aws.String("us-east-1"),
-			Service:           aws.String("EC2"),
-			StatusCode:        aws.String("open"),
-		},
-		&health.Event{
-			EventTypeCategory: aws.String("issue"),
-			Region:            aws.String("us-east-1"),
-			Service:           aws.String("LAMBDA"),
-			StatusCode:        aws.String("closed"),
-		},
-		&health.Event{
-			EventTypeCategory: aws.String("issue"),
-			Region:            aws.String("us-east-1"),
-			Service:           aws.String("LAMBDA"),
-			StatusCode:        aws.String("closed"),
-		},
-		&health.Event{
-			EventTypeCategory: aws.String("issue"),
-			Region:            aws.String("us-east-1"),
-			Service:           aws.String("LAMBDA"),
-			StatusCode:        aws.String("closed"),
-		},
-	}
+	var statuses = []*ec2.InstanceStatus{}
+	/*		&health.Event{
+				EventTypeCategory: aws.String("issue"),
+				Region:            aws.String("eu-west-1"),
+				Service:           aws.String("EC2"),
+				StatusCode:        aws.String("open"),
+			},
+			&health.Event{
+				EventTypeCategory: aws.String("issue"),
+				Region:            aws.String("us-east-1"),
+				Service:           aws.String("EC2"),
+				StatusCode:        aws.String("open"),
+			},
+			&health.Event{
+				EventTypeCategory: aws.String("issue"),
+				Region:            aws.String("us-east-1"),
+				Service:           aws.String("LAMBDA"),
+				StatusCode:        aws.String("closed"),
+			},
+			&health.Event{
+				EventTypeCategory: aws.String("issue"),
+				Region:            aws.String("us-east-1"),
+				Service:           aws.String("LAMBDA"),
+				StatusCode:        aws.String("closed"),
+			},
+			&health.Event{
+				EventTypeCategory: aws.String("issue"),
+				Region:            aws.String("us-east-1"),
+				Service:           aws.String("LAMBDA"),
+				StatusCode:        aws.String("closed"),
+			},
+		}*/
 	e := &exporter{
-		api:    &mockHealthAPI{events: events},
-		client: &mockEC2Client{},
-		filter: &health.EventFilter{},
+		client: &mockEC2Client{statuses: statuses},
 	}
-
-	gv := prometheus.NewGaugeVec(eventOpts, labels)
-	e.scrapeHealth(gv)
-
-	validateMetric(t, gv, events[0], 1.)
-	validateMetric(t, gv, events[1], 1.)
-	validateMetric(t, gv, events[2], 3.)
+	e.Collect(nil)
+	//validateMetric(t, gv, events[0], 1.)
+	//validateMetric(t, gv, events[1], 1.)
+	//validateMetric(t, gv, events[2], 3.)
 }
 
+/*
 func validateMetric(t *testing.T, vec *prometheus.GaugeVec, e *health.Event, expectedVal float64) {
 	m := vec.WithLabelValues(*e.EventTypeCategory, *e.Region, *e.Service, *e.StatusCode)
 	pb := &dto.Metric{}
@@ -92,4 +72,4 @@ func validateMetric(t *testing.T, vec *prometheus.GaugeVec, e *health.Event, exp
 	if pb.GetGauge().GetValue() != expectedVal {
 		t.Errorf("Invalid value - Expected: %v Got: %v", expectedVal, val)
 	}
-}
+} */
